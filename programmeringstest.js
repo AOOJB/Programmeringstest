28872052b0fc81acbf45222e447bf8cbdcfdd0f9
AOOJB <antonjb9@gmail.com>
2020-12-01 09:47:17 +0100

    laptop

---
+++ programmeringstest.js
@@ -0,0 +1,62 @@
+//Uppgift 1
+console.log("Svaret på Uppgift 1:")
+
+//Uppgift 2
+var textMessage = "This is an example text which will be used later for lateral purposes"
+var searchMessage = "la"
+var firstNum = 650
+var secondNum = 13
+var thirdNum = 60
+var primes = [1,2]
+var fibonacci = [0,1]
+var scrambledNumbers = [3, -65, 102, 5, -34, -12, -98, 93, 67, 76, 32, -1, 0, 48, 18]
+
+//Blandade uppgifter
+
+//1
+var tal1 = 5
+var tal2 = 3
+function add(tal1, tal2){
+	return tal1 + tal2;
+}
+
+//2
+var a;
+for(a = 1;a <= 1024; a=a*2){
+	console.log(a)
+}
+
+//3
+var b;
+for (b =1; b <=21; b=b+2){
+	console.log(b)
+}
+
+//4
+if(firstNum > secondNum){
+	console.log(firstNum)
+}
+
+//5
+//var tal = prompt("Input")
+function kvadrat(tal){
+}
+
+//6
+var tal6;
+for(tal6 = 1; tal6 <=20; tal6 ++){
+	console.log(tal6)
+	if(tal6 == 7){
+		console.log("jämnt delbart med 7")
+	}
+	if(tal6 == 14){
+		console.log("jämnt delbart med 7")
+	}
+	
+}
+
+//7
+//var bokstäver;
+//for(bokstäver = 1; bokstäver <= 28; a++){
+	//console.log(bokstäver)
+//}
